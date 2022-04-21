cd50ace261e2d5d5c0a7d3f2ffbafefc53fff9c7
Ivar <ivar.nesje@finanstilsynet.no>
2022-04-21 21:23:11 +0200

    Silly draft for diff view

--- altinn3editor/src/features/diff/DiffView.tsx
+++ altinn3editor/src/features/diff/DiffView.tsx
@@ -1,6 +1,16 @@
-import ZipDownload  from "../zip/ZipDownload";
+import { diff } from "deep-object-diff";
+import { useAppSelector } from "../../redux/hooks";
+import ZipDownload from "../zip/ZipDownload";
 
-export default function Diff(){
-    // TODO: actually show a diff in browser
-    return <div><ZipDownload /></div>
+export default function Diff() {
+  const initialRepo = useAppSelector((s) => s.initialRepo);
+  const currentRepo = useAppSelector((s) => s.currentRepo);
+  const differences = diff(initialRepo, currentRepo);
+  // TODO: actually show a diff in browser
+  return (
+    <div>
+      <ZipDownload />
+      <pre>{JSON.stringify(differences, null, 4)}</pre>
+    </div>
+  );
 }
